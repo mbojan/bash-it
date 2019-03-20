45bfba30dc623989934dc446a395202dca53b40b
Michal Bojanowski <michal2992@gmail.com>
2019-03-20 14:21:49 +0100

    Update R plugin
    Better info message of update-packages.
    Add rstudio-start

--- plugins/available/r.plugin.bash
+++ plugins/available/r.plugin.bash
@@ -8,6 +8,7 @@ function update-packages {
 	# Get number of cores
 	local NCORES=$(grep -c ^processor /proc/cpuinfo)
 	local USE_CORES=$(expr $NCORES - 1)
+	echo Updating R packages in $(Rscript -e 'cat(.libPaths()[1])') using $USE_CORES cores
 	MAKE='make -j $USE_CORES' Rscript -e 'update.packages(ask=FALSE, lib=.libPaths()[1])'
 }
 
@@ -19,3 +20,10 @@ function autotest-package {
 	Rscript -e 'testthat::auto_test_package()'
 }
 
+
+function rstudio-start {
+	about 'start rstudio server'
+	group 'r'
+
+	service rstudio-server start
+}
