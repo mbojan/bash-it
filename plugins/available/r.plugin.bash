e80bab0e2fc80eb08a8bf885eee4bba4bc179790
Michal Bojanowski <michal2992@gmail.com>
2019-04-05 16:58:37 +0200

    Rename R-related functions

--- plugins/available/r.plugin.bash
+++ plugins/available/r.plugin.bash
@@ -1,19 +1,21 @@
 cite about-plugin
 about-plugin 'R specific functions'
 
-function update-r-packages {
+
+function rpkg-update {
 	about 'update packages in .libPaths()[1] library'
 	group 'r'
 
 	# Get number of cores
 	local NCORES=$(grep -c ^processor /proc/cpuinfo)
 	local USE_CORES=$(expr $NCORES - 1)
+
 	echo Updating R packages in $(Rscript -e 'cat(.libPaths()[1])') using $USE_CORES cores
 	MAKE='make -j $USE_CORES' Rscript -e 'update.packages(ask=FALSE, lib=.libPaths()[1])'
 }
 
 
-function autotest-package {
+function rpkg-autotest {
 	about 'launch package autocheck from testthat'
 	group 'r'
 
@@ -28,3 +30,18 @@ function rstudio-start {
 	echo Launch RStudio Server with sudo
 	sudo service rstudio-server start
 }
+
+
+function rpkg-install {
+	about 'install R package parallelled'
+	param '1: name of the package to install'
+	group 'r'
+
+	# Get number of cores
+	local NCORES=$(grep -c ^processor /proc/cpuinfo)
+	local USE_CORES=$(expr $NCORES - 1)
+
+	echo Installing package $1 using $USE_CORES cores
+	MAKE="make -j$USE_CORES" Rscript -e "install.packages(\"$1\")"
+}
+
