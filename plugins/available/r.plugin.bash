b4e67d4c4d48c7b33c0f9325bafaf414fb180beb
Michal Bojanowski <michal2992@gmail.com>
2020-02-10 14:20:06 +0100

    Update R-related scripts

--- plugins/available/r.plugin.bash
+++ plugins/available/r.plugin.bash
@@ -1,7 +1,12 @@
 cite about-plugin
 about-plugin 'R specific functions'
 
-function rpkg-u {
+
+# R administration
+#
+# Package management and so on.
+
+function radmin-update {
 	about 'update R packages'
 	group 'r'
 
@@ -9,107 +14,40 @@ function rpkg-u {
 		echo ""
 		echo "Update R packages in specified library"
 		echo ""
-		echo "Usage: rpkg-u [-l <string>] [-p <integer>]" 1>&2
+		echo "Usage: radmin-update [-l <string>] [-p <integer>]" 1>&2
 		echo ""
-		echo "-l  Library path. Defaults to .libPaths()[1]"
+		echo "-l  Library path number as returned by .libPaths(). Defaults to 1 so the packages"
+	        echo "    are updated in .libPaths()[1]"
 		echo "-p  Number of cores. Defaults to 1"
+		echo "-h  Display this message"
 		echo ""
 	}
 
-	while getopts ":l:p:" o; do
+	# Default option values
+	local l=1
+	local p=1
+	echo $*
+
+	while getopts ":l:p:h" o; do
 		case "${o}" in
-			l)
-				local l=${OPTARG}
-				;;
-			p)
-				local p=${OPTARG}
-				;;
+			l) local l=${OPTARG} ;;
+			p) local p=${OPTARG} ;;
+			h) 
+				usage 
+				exit 0;;
 			*)
 				usage
-				;;
+				exit 1;;
 		esac
 	done
-	shift $((OPTIND-1))
+	shift $((OPTIND - 1))
 
-	if [ -z "${l}" ]; then
-		local l=1
-	fi
-
-	if [ -z "${p}" ]; then
-		local p=1
-	fi
-
-	echo "l = ${l}"
-	echo "p = ${p}"
-	
-	echo "MAKE='make -j${p}' Rscript -e 'update.packages(lib=.libPaths()[${l}], ask=FALSE, checkBuilt=TRUE)'"
+	echo "Updating packages in 1:$(Rscript -e "cat(.libPaths()[${l}])") using ${p} core(s)."
+	read -n 2 -p "Press any key to continue"
+	MAKE="make -j${p}" Rscript -e "update.packages(lib=.libPaths()[${l}], ask=FALSE, checkBuilt=TRUE)"
 }
 
-
-function rpkg-github {
-	about 'install R package from GitHub'
-	param '1: user/repo string to GH repository with the package'
-	group 'r'
-
-	Rscript -e "remotes::install_github(\"$1\", build_vignettes=TRUE, force=TRUE)"
-}
-
-
-function rpkg-update {
-	about 'update packages in .libPaths()[1] library with parallel compilation'
-	group 'r'
-
-	# Get number of cores
-	local NCORES=$(grep -c ^processor /proc/cpuinfo)
-	local USE_CORES=$(expr $NCORES - 1)
-
-	echo Updating R packages in $(Rscript -e 'cat(.libPaths()[1])') using $USE_CORES cores
-	MAKE='make -j $USE_CORES' Rscript -e 'update.packages(ask=FALSE, lib=.libPaths()[1], checkBuilt=TRUE)'
-}
-
-function rpkg-update1 {
-	about 'update packages in .libPaths()[1] library using one core'
-	group 'r'
-
-	Rscript -e 'update.packages(ask=FALSE, lib=.libPaths()[1])'
-}
-
-function rpkg-update2 {
-	about 'update packages in .libPaths()[1] library using two cores'
-	group 'r'
-
-	MAKE='make -j2' Rscript -e 'update.packages(ask=FALSE, lib=.libPaths()[1])'
-}
-
-function rpkg-updatep {
-	about 'update packages in .libPaths()[1] library in parallel'
-	group 'r'
-
-	# Get number of cores
-	local NCORES=$(grep -c ^processor /proc/cpuinfo)
-	local USE_CORES=$(expr $NCORES - 1)
-
-	Rscript -e "update.packages(ask=FALSE, lib=.libPaths()[1], Ncpus=$USE_CORES, checkBuilt=TRUE)"
-}
-
-function rpkg-autotest {
-	about 'launch package autocheck from testthat'
-	group 'r'
-
-	Rscript -e 'testthat::auto_test_package()'
-}
-
-
-function rstudio-start {
-	about 'start rstudio server'
-	group 'r'
-
-	echo 'Launch RStudio Server';
-	service rstudio-server start
-}
-
-
-function rpkg-install {
+function radmin-install {
 	about 'install R package parallelled'
 	param '1: name of the package to install'
 	group 'r'
@@ -122,12 +60,62 @@ function rpkg-install {
 	MAKE="make -j$USE_CORES" Rscript -e "install.packages(\"$1\")"
 }
 
-function rpkg-old {
+function radmin-github {
+	about 'install R package from GitHub'
+	param '1: user/repo string to GH repository with the package'
+	group 'r'
+
+	Rscript -e "remotes::install_github(\"$1\", build_vignettes=TRUE, force=TRUE)"
+}
+
+
+function radmin-oldpkg {
 	about 'check for old packages'
 	group 'r'
 
 	Rscript -e 'old.packages()'
 }
+
+
+
+
+
+
+
+# Package development
+#
+# Running functions from 'devtools' and so on.
+
+
+function rpkg-autotest {
+	about 'launch package autocheck from testthat'
+	group 'r'
+
+	Rscript -e 'testthat::auto_test_package()'
+}
+
+function rpkg-install {
+	about 'run devtools::install()'
+	group r
+
+	echo 'Running devtools::install()'
+	Rscript -e 'devtools::install()'
+}
+
+
+
+
+
+# Other stuff
+
+function rstudio-start {
+	about 'start rstudio server'
+	group 'r'
+
+	echo 'Launch RStudio Server';
+	service rstudio-server start
+}
+
 
 
 function r-render {
@@ -145,10 +133,3 @@ function r-here {
 	cd `Rscript -e 'cat(here::here())'`
 }
 
-function r-install {
-	about 'run devtools::install()'
-	group r
-
-	echo 'Running devtools::install()'
-	Rscript -e 'devtools::install()'
-}
