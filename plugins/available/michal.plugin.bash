8d15a87110fff46227ea5890d718ef98596f93c8
Michal Bojanowski <michal2992@gmail.com>
2020-09-24 17:54:16 +0200

    Fix function launching CDDA

--- plugins/available/michal.plugin.bash
+++ plugins/available/michal.plugin.bash
@@ -34,3 +34,13 @@ function dcss {
            new-session links http://crawl.chaosforge.org/Crawl_Wiki \; \
            new-window crawl \; 
 }
+
+function cddab {
+	about 'Launch CDDA in byobu in tmux with wiki in background'
+	group 'michal'
+
+        byobu  \
+           new-session links http://cddawiki.chezzo.com/cdda_wiki/index.php?title=Main_Page \; \
+           new-window links http://cdda.chezzo.com/ \; \
+           new-window cdda \; 
+}
