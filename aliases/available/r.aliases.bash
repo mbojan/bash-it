2375296e33db1fab2dc97f0306b783195f9b3e90
Michal Bojanowski <michal2992@gmail.com>
2020-11-13 00:37:48 +0100

    Aliases dev start with rpkg-

--- aliases/available/r.aliases.bash
+++ aliases/available/r.aliases.bash
@@ -4,6 +4,6 @@ about-alias 'R abbreviations'
 # Aliases
 alias r-oldpkg='Rscript -e "old.packages()"'
 alias r-updatep='Rscript -e "update.packages(lib=.libPaths()[1], ask=FALSE, Ncpus=2)"'
-alias r-autotest='Rscript -e "testthat::auto_test_package()"'
-alias r-install='Rscript -e "devtools::install(build_vignettes=TRUE)"'
+alias rpkg-autotest='Rscript -e "testthat::auto_test_package()"'
+alias rpkg-install='Rscript -e "devtools::install(build_vignettes=TRUE)"'
 alias r-here='cd `Rscript -e "cat(here::here())"`'
