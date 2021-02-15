78c8d8169fa976025522f277df56ec912d2d7187
Michal Bojanowski <michal2992@gmail.com>
2021-02-15 21:46:50 +0100

    Add rpkg-build

--- aliases/available/r.aliases.bash
+++ aliases/available/r.aliases.bash
@@ -4,6 +4,7 @@ about-alias 'R abbreviations'
 # Aliases
 alias r-oldpkg='Rscript -e "old.packages()"'
 alias r-update2='Rscript -e "update.packages(lib=.libPaths()[1], ask=FALSE, Ncpus=2)"'
+alias r-here='cd `Rscript -e "cat(here::here())"`'
 alias rpkg-autotest='Rscript -e "testthat::auto_test_package()"'
 alias rpkg-install='Rscript -e "devtools::install(build_vignettes=TRUE)"'
-alias r-here='cd `Rscript -e "cat(here::here())"`'
+alias rpkg-build='Rscript -e "devtools::build()"'
